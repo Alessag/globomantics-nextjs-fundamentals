8ca991b6d9478c36440b4d60eb53e85866cc0051
Alessandra Amicarella <alessamica@gmail.com>
2023-05-17 16:14:16 -0400

    Dinamic routing

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,15 +1,12 @@
+import styles from "../conference.module.css";
 import Link from "next/link";
 
-// Static site generation with revalidation
+// Static data fetching
 async function fetchSpeakers() {
   const response = await fetch(
-    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
-    {
-      next: {
-        revalidate: 20,
-      },
-    }
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json"
   );
+
   const data = await response.json();
   return data;
 }
@@ -18,19 +15,19 @@ export default async function Page() {
   const data = await fetchSpeakers();
 
   return (
-    <div className="border-2 border-black max-w-6xl m-auto">
-      <h1 className="font-bold text-5xl text-center my-4">
-        Welcome to Globomantics Speakers
-      </h1>
-      <div>Last time updated: {new Date().toLocaleTimeString()}</div>
-      <div className="grid grid-cols-2 gap-5">
-        {data.speakers.map(({ id, name, bio }) => (
-          <div className="border-2 border-pink-300" key={id}>
-            <h2 className="font-semibold uppercase text-lg my-2">{name}</h2>
-            <p className="text-justify">{bio}</p>
-          </div>
-        ))}
+    <div className={styles.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
       </div>
+      <h1>Welcome to Globomantics Speakers</h1>
+      {data.speakers.map(({ id, name, bio }) => (
+        <div key={id} className={styles.infoContainer}>
+          <Link className={styles.bgLinks} href={`/conference/speakers/${id}`}>
+            <h3 className={styles.titleText}>{name}</h3>
+          </Link>
+          <h5 className={styles.descText}>{bio}</h5>
+        </div>
+      ))}
     </div>
   );
 }
