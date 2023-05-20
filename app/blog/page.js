53eaa02dc8c691dffd56a8aa92353f87225529c6
Alessandra Amicarella <alessamica@gmail.com>
2023-05-19 22:38:42 -0400

    feat: an

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
 
-export default function Page() {
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  return data;
+}
+
+export default async function Page() {
+  const posts = await fetchPosts();
+  console.log(posts);
   return (
     <>
       <h1>Welcome to Blog Page</h1>
