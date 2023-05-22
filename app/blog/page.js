6017b0d17b1be9626686dc2eb5c8c09a1368b420
Alessandra Amicarella <alessamica@gmail.com>
2023-05-21 21:52:39 -0400

    test

--- app/blog/page.js
+++ app/blog/page.js
@@ -15,6 +15,7 @@ export default async function Page() {
       <h1>Welcome to Blog Page</h1>
       <h2>
         <Link href="/home">Back to Home</Link>
+        gdfgdfgfd
       </h2>
     </>
   );
